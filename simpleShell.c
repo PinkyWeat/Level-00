d7019178c6f545c3e73a94523aa64439c0498ca1
Denisse Landau <denisselandau97@gmail.com>
2022-07-27 05:13:53 -0700

    interactive

--- simpleShell.c
+++ simpleShell.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 /**
- *
+ * main - interactive simple shell.
+ * Return: return 0 on success, -1 error.
  */
 int main(void)
 {
-	/* char *args = {" ", NULL}; */
+	char *args[] = {" ", NULL};
 	size_t characters;
 	size_t bufsize = 32;
 	char *buffer = malloc(bufsize * sizeof(char));
@@ -14,9 +16,16 @@ int main(void)
 	printf("#cisfun$ ");
 	/* gets line from stream */
 	characters = getline(&buffer,&bufsize, stdin);
-	/* ignore \n */
-	printf("%zu\n", (characters - 1));
-	printf("%s", buffer);
+	/* ignore \n  with -1 */
+	/* printf("%zu\n", (characters - 1)); prints n of char printed */
+	/* printf("%s", buffer); prints what was written in cmd line */
+	buffer = strtok(buffer, "\n"); /* uses \n to cut */
+	args[0] = buffer; /* route */
+	if (execve(args[0], args, NULL) == -1)
+	{
+		perror("Error: ");
+		return (-1);
+	}
 
 	free(buffer);
 	return (0);
